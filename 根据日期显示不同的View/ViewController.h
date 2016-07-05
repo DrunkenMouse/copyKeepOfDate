e197d0c73225a96da9a3d11026616c5510352d5d
残月 <632342527@qq.com>
2016-07-05 15:59:44 +0800

    Initial Commit

---
+++ 根据日期显示不同的View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  根据日期显示不同的View
+//
+//  Created by 王奥东 on 16/7/5.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 根据日期显示不同的View/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  根据日期显示不同的View
+//
+//  Created by 王奥东 on 16/7/5.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
